5f20fac1228109851fef146bd97f465e91c24193
goncalvespedroo <contato.pedrojson@gmail.com>
2026-05-27 07:33:44 -0300

    feat(auth): add user registration with bcrypt hashing and data sanitization

--- apps/api/src/controllers/AuthController.ts
+++ apps/api/src/controllers/AuthController.ts
@@ -1,11 +1,11 @@
-import { DataSource } from "typeorm";
 import { Request, Response } from "express";
 import User from "../entities/Users";
 import { AppDataSource } from "../database";
+import bcrypt from "bcrypt";
 
 export class AuthController {
   async register(req: Request, res: Response) {
-    const { nome, email, senha } = req.body;
+    const { name, email, password } = req.body;
     const userRepository = AppDataSource.getRepository(User);
 
     const existingUser = await userRepository.findOneBy({ email: email });
@@ -13,5 +13,20 @@ export class AuthController {
     if (existingUser) {
       return res.status(400).json({ message: "This email is already in use." });
     }
+
+    const saltRound = 10;
+    const passwordHash = await bcrypt.hash(password, saltRound);
+
+    const newUser = userRepository.create({
+      name,
+      email,
+      password: passwordHash,
+    });
+
+    await userRepository.save(newUser);
+
+    const { password: _, ...userWithoutPassword } = newUser;
+
+    return res.status(201).json(userWithoutPassword);
   }
 }
