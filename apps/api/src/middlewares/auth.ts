a524e857492879d7d0e04c44ed454e4cc1d16152
goncalvespedroo <contato.pedrojson@gmail.com>
2026-05-31 18:34:44 -0300

    feat(auth): implement tonken extraction and verification using jwt

--- apps/api/src/middlewares/auth.ts
+++ apps/api/src/middlewares/auth.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 export const authMiddleware = (
   req: Request,
@@ -6,4 +7,17 @@ export const authMiddleware = (
   next: NextFunction,
 ) => {
   const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ");
+
+  try {
+    jwt.verify(token[1], "secret");
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid tonken" });
+  }
 };
